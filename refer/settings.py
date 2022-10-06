9940e7a997a0e3d13fd2285442e3dc857884e9a2
Yuvraj Jwala <yuvrajjwala@gmail.com>
2022-10-06 20:40:05 +0530

    mac - 13 responsive done

--- refer/settings.py
+++ refer/settings.py
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'refer.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-DATABASES['default'] = dj_database_url.config(default=env('DATABASE_URL'),conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(default=env('DATABASE_URL'),conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
